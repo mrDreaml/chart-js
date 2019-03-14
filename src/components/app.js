ca29f9d7d5968b6442058aa6ead9d865ce6cdb20
DimaPlus <dimaandreev0101@gmail.com>
2019-03-14 22:19:07 +0300

    add: updating on resize

--- src/components/app.js
+++ src/components/app.js
@@ -6,28 +6,18 @@ import chartInputData from '../data/index';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.chartSVGProps = {
-      id: 'chartJS-container', height: '500px', width: '100%', position: 'relative',
-    };
-    this.state = {
-      svgIsLoaded: false,
-    };
   }
 
   componentDidMount() {
-    this.setState({
-      svgIsLoaded: true,
-    });
+
   }
 
   render() {
-    const { chartSVGProps } = this;
-    const { svgIsLoaded } = this.state;
-
+    const chartSVGProps = {
+      id: 'chartJS-container', height: '500px', width: '100%', position: 'relative',
+    };
     return (
-      <svg {...chartSVGProps}>
-        { svgIsLoaded ? <ChartJS inputData={chartInputData[0]} containerID={chartSVGProps.id} /> : <span />}
-      </svg>
+      <ChartJS inputData={chartInputData[0]} chartSVGProps={chartSVGProps} />
     );
   }
 }
--- src/components/chart/chartJS.js
+++ src/components/chart/chartJS.js
@@ -9,8 +9,22 @@ import Notification from './Notification';
 class ChartJS extends Component {
   constructor(props) {
     super(props);
-    const { containerID } = this.props; // eslint-disable-line
 
+    this.state = {
+      notification: {
+        isShow: false,
+        pos: [0, 0],
+      },
+      isSvgDidMout: false,
+    };
+
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+
+    const { id: containerID } = this.props.chartSVGProps; // eslint-disable-line
     const chartContainer = document.getElementById(containerID);
     this.containerWidth = chartContainer.clientWidth;
     this.containerHeight = chartContainer.clientHeight;
@@ -25,22 +39,24 @@ class ChartJS extends Component {
 
     const { columns } = this.props.inputData; // eslint-disable-line
     Object.assign(this.chartParams, getAdditionalChartParams.call(this.chartParams, columns));
-
-    this.state = {
-      notification: {
-        isShow: false,
-        pos: [0, 0],
-      },
-    };
+    this.setState({
+      isSvgDidMout: true,
+    });
   }
 
-  componentDidMount() {
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
   }
+
+  updateDimensions() {
+    this.componentDidMount();
+  }
+
 
   showNotification(e) {
     const posChartX = e.pageX - this.containerOffsetLeft;
     const posChartY = e.pageY;
-    console.log(posChartX, posChartY);
+    // console.log(posChartX, posChartY);
     this.setState({
       notification: {
         isShow: true,
@@ -59,7 +75,7 @@ class ChartJS extends Component {
 
   render() {
     const { columns } = this.props.inputData; // eslint-disable-line
-    const { notification } = this.state;
+    const { notification, isSvgDidMout } = this.state;
 
     const chartBgStyle = {
       fill: '#EEEEEE',
@@ -83,19 +99,28 @@ class ChartJS extends Component {
       },
     };
 
-    const { width, height } = this.chartParams;
+    const { chartSVGProps } = this.props;
+
     return (
-      <React.Fragment>
-        <rect width={width} height={height} style={chartBgStyle} onMouseMove={this.showNotification.bind(this)} onMouseLeave={this.removeNotification.bind(this)} />
-        <ChartAxisX chartParams={this.chartParams} chartColumnValues={columns.x} styles={chartAxisStyle.textStyle} />
-        <ChartAxisY chartParams={this.chartParams} styles={chartAxisStyle} />
-        <Chart chartParams={this.chartParams} chartColumnValues={columns.y0} styles={chartLineStyle} />
-        <Chart chartParams={this.chartParams} chartColumnValues={columns.y1} styles={chartLineStyle2} />
-        {notification.isShow ? <Notification /> : null }
-      </React.Fragment>
+      <svg {...chartSVGProps} onMouseMove={this.showNotification.bind(this)} onMouseLeave={this.removeNotification.bind(this)}>
+        { isSvgDidMout
+          ? (
+            <React.Fragment>
+              <rect width={this.chartParams.width} height={this.chartParams.height} style={chartBgStyle} />
+              <ChartAxisX chartParams={this.chartParams} chartColumnValues={columns.x} styles={chartAxisStyle.textStyle} />
+              <ChartAxisY chartParams={this.chartParams} styles={chartAxisStyle} />
+              <Chart chartParams={this.chartParams} chartColumnValues={columns.y0} styles={chartLineStyle} />
+              <Chart chartParams={this.chartParams} chartColumnValues={columns.y1} styles={chartLineStyle2} />
+              {notification.isShow ? <Notification /> : null }
+            </React.Fragment>
+          )
+          : null }
+      </svg>
     );
   }
 }
 
 
 export default ChartJS;
+
+// svg => shold be there
