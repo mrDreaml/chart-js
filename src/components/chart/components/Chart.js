6a49235bccbc24fd080cb3d721666215b9c2df3b
mrDreaml <dimaandreev0101@gmail.com>
2019-10-01 00:00:30 +0300

    !2.0 feat performance boost

--- src/components/chart/components/Chart.js
+++ src/components/chart/components/Chart.js
@@ -1,56 +1,27 @@
 import React from 'react';
 import constants from '../../constants/constants';
 
-const Line = ({
-  chartParams, chartColumnValues, styles,
+
+export default ({
+  chartColumnsShow, currentColumnValues, colors, chartParams,
 }) => {
   const {
-    xStep, yStep, chartHeight,
+    chartTop, chartLeft, chartHeight, yStep, xStep,
   } = chartParams;
-  let { chartLeft, chartTop } = chartParams;
-
-  if (chartLeft === undefined) {
-    chartLeft = 0;
-  }
-  if (chartTop === undefined) {
-    chartTop = 0;
-  }
-  const renderData = chartColumnValues.map((e, i) => {
-    const x1Coordinate = chartLeft + (i - 1) * xStep;
-    const x2Coordinate = chartLeft + i * xStep;
-    const y1Coordinate = chartTop + chartHeight - chartColumnValues[i - 1] * yStep;
-    const y2Coordinate = chartTop + chartHeight - chartColumnValues[i] * yStep;
-    const key = `${x1Coordinate + x2Coordinate + y1Coordinate + y2Coordinate + i}chart`;
-    return i !== 0
-      ? (<line className={`chartLine ${i}`} key={key} x1={x1Coordinate} y1={y1Coordinate} x2={x2Coordinate} y2={y2Coordinate} {...styles} />) : null;
-  });
-  return renderData;
+  const dPaths = Object.entries(currentColumnValues).reduce((path, [colName, colValue]) => {
+    if (colValue !== null && colName !== constants.colNameX && chartColumnsShow[colName]) {
+      path[colName] = (currentColumnValues[colName].reduce((d, yValue, xValue) => {
+        const yCoordinate = Math.round(chartTop + chartHeight - yValue * yStep);
+        const xCoordinate = Math.round(chartLeft + xValue * xStep);
+        if (xValue === 0) {
+          return `M${xCoordinate} ${yCoordinate}`;
+        }
+        return d.concat(` L${xCoordinate} ${yCoordinate}`);
+      }, ''));
+    }
+    return path;
+  }, {});
+  return (
+    Object.entries(dPaths).map(([colName, dPath]) => <path d={dPath} stroke={colors[colName]} className="chart--path" />)
+  );
 };
-
-
-export default ({ chartColumnsShow, currentColumnValues, colors, chartParams }) => (
-  Object.values(chartColumnsShow).includes(true)
-    ? (
-      <>
-        {Object.entries(currentColumnValues).map((col, i) => {
-          const [colName, colValue] = col;
-          if (colValue !== null && colName !== constants.colNameX) {
-            const chartStyle = {
-              stroke: colors[colName],
-            };
-            const key = colName + colValue[i];
-            return (
-              <Line
-                key={key}
-                chartParams={chartParams}
-                chartColumnValues={colValue}
-                styles={chartStyle}
-              />
-            );
-          }
-          return null;
-        })}
-      </>
-    )
-    : null
-);
